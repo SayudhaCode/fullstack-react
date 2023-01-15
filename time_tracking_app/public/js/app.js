9580ed1704eb35271358d97106429f25b5e49086
sayudha <mazaya.studio@outlook.com>
2023-01-15 14:09:33 +0700

    Start Step 6: Add inverse data flow, `halaman 134`

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -51,10 +51,15 @@ class EditableTimerList extends React.Component {
 }
 
 class EditableTimer extends React.Component {
+	state = {
+		editFormOpen: false,
+	};
+
 	render() {
 		if (this.props.editFormOpen) {
 			return (
 					<TimerForm
+							id={ this.props.id }
 							title={ this.props.title }
 							project={ this.props.project }
 					/>
@@ -62,6 +67,7 @@ class EditableTimer extends React.Component {
 		} else {
 			return (
 					<Timer
+							id={ this.props.id }
 							title={ this.props.title }
 							project={ this.props.project }
 							elapsed={ this.props.elapsed }
@@ -73,6 +79,19 @@ class EditableTimer extends React.Component {
 }
 
 class TimerForm extends React.Component {
+	state = {
+		title: this.props.title || '',
+		project: this.props.project || '',
+	};
+
+	handleTitleChange = e => {
+		this.setState({title: e.target.value});
+	};
+
+	handleProjectChange = e => {
+		this.setState({project: e.target.value});
+	};
+
 	render() {
 		const submitText = this.props.title ? 'Update' : 'Create';
 		return (
@@ -81,11 +100,19 @@ class TimerForm extends React.Component {
 						<div className='ui form'>
 							<div className='field'>
 								<label>Title</label>
-								<input type='text' defaultValue={ this.props.title } />
+								<input
+										type='text'
+										value={ this.props.title }
+										onChange={ this.handleTitleChange }
+								/>
 							</div>
 							<div className='field'>
 								<label>Project</label>
-								<input type='text' defaultValue={ this.props.project } />
+								<input
+										type='text'
+										value={ this.props.project }
+										onChange={ this.handleProjectChange }
+								/>
 							</div>
 							<div className='ui two bottom attached buttons'>
 								<button className='ui basic blue button'>
@@ -103,15 +130,25 @@ class TimerForm extends React.Component {
 }
 
 class ToggleableTimerForm extends React.Component {
+	state = {
+		isOpen: false,
+	};
+	handleFormOpen = () => {
+		this.setState({isOpen: true});
+	};
+
 	render() {
-		if (this.props.isOpen) {
+		if (this.state.isOpen) {
 			return (
 					<TimerForm />
 			);
 		} else {
 			return (
 					<div className='ui basic content center aligned segment'>
-						<button className='ui basic button icon'>
+						<button
+								className='ui basic button icon'
+								onClick={ this.handleFormOpen }
+						>
 							<i className='plus icon'></i>
 						</button>
 					</div>
